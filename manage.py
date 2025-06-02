d0c345c293ca193c4f710ee443723a334aabcb0b
yasin uysal <uysalyasin25@gmail.com>
2025-06-02 12:46:28 +0300

    Render için IP ve port ayarlandı

--- manage.py
+++ manage.py
@@ -15,6 +15,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+import os
+if 'PORT' in os.environ:
+    sys.argv += ['runserver', '0.0.0.0:' + os.environ['PORT']]
+
     execute_from_command_line(sys.argv)
 
 
