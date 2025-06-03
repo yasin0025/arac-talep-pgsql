769f656f655177dbc2a44511736b305a118d8f28
yasin uysal <uysalyasin25@gmail.com>
2025-06-03 11:05:24 +0300

    weasyprint eklendi, wsgi yolu düzeltildi

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-gunicorn projeadi.wsgi:application --bind 0.0.0.0:8000
+gunicorn arac_talep_sistemi.wsgi:application --bind 0.0.0.0:8000
+
