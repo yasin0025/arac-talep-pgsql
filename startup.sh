fcc46d33c6a3ac1518fe317a8a23d4b70bab3a47
yasin uysal <uysalyasin25@gmail.com>
2025-07-11 09:38:02 +0300

    migrate otomatik çalıştırıldı

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-python manage.py migrate
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-gunicorn arac_talep_sistemi.wsgi:application --bind 0.0.0.0:8000
-python manage.py createsuperuser --noinput --username admin --email admin@example.com
-
+gunicorn arac_talep_sistemi.wsgi:application
